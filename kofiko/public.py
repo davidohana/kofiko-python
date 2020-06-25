6c62494d94ed80bf45d4b62ce2bd26d5d6f31a80
davidoh <david.ohana@ibm.com>
2020-06-25 16:14:09 +0300

    code doc

--- kofiko/public.py
+++ kofiko/public.py
@@ -2,6 +2,7 @@
 (Ko)de (Fi)rst (Ko)nfiguration
 A configuration package with code-first approach for Python.
 Copyright (C) 2020 David Ohana @ ibm.com
+License: Apache-2.0
 """
 
 import importlib
@@ -14,9 +15,9 @@ from kofiko.internal import *
 def config_section(section_name: str = ""):
     """
     Mark a class as a config section. Class will be registered once the containing module is executed.
+    Make sure module with decorated class is loaded by importing it before calling :func:`kofiko.configure`.
 
-    :param section_name:
-    :return:
+    :param section_name: name of the section that will be used for lookups in ini/env. Class name is used when empty.
     """
     # allow decorators without arguments, first arg is the class
     if inspect.isclass(section_name):
@@ -33,6 +34,14 @@ def config_section(section_name: str = ""):
 
 
 def config_custom(customization_name=""):
+    """
+    Mark a function as a customization. Function will be registered once the containing module is executed.
+    Customization function is called if :func:`kofiko.configure` is called with the same customization name.
+    Multiple functions can be registered for the same customization name (they all will be executed)
+    Make sure module with decorated class is loaded by importing it before calling :func:`kofiko.configure`.
+
+    :param customization_name: name of the customization. Function name is used when empty.
+    """
     # allow decorators without arguments, first arg is the function
     if callable(customization_name):
         func = customization_name
@@ -47,14 +56,25 @@ def config_custom(customization_name=""):
 
 
 def register_section(cls, name=""):
+    """
+    Explicitly register a configuration class as a section if you are unable to use `config_section` decorator.
+    """
     config_sections.append(ConfigObject(cls, name))
 
 
 def register_customization(func, name=""):
+    """
+    Explicitly register a customization function if you are unable to use `config_custom` decorator.
+    """
     config_customizations.append(ConfigObject(func, name))
 
 
 def register_module(module, recursive=False):
+    """
+    Ensure module is loaded, so that decorators inside that module are activated.
+    :param module:
+    :param recursive: load also sub-modules.
+    """
     if not recursive:
         # do nothing, thus function just makes sure that the module is loaded
         # before init() is called, to force registration of inner config objects
@@ -68,6 +88,9 @@ def register_module(module, recursive=False):
 
 
 def get_section_as_dict(cls):
+    """
+    Convert public static attributes of the called class into kay-value elements in a dictionary.
+    """
     attr_dict = {}
     for name, val in cls.__dict__.items():
         if name.startswith("__"):
